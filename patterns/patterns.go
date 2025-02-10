aae64a8926519237eb410aa50909909017dffe8c
Etherial <typicaluserinfinite@gmail.com>
2025-02-10 16:06:52 +0900

    add struct pattern and reciever function generate pattern

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -4,7 +4,11 @@ import (
 	"math/rand"
 )
 
-func GenerateLeftSideIndexes(dimensions int) []int {
+type Pattern struct {
+	Left, Middle, Right []int
+}
+
+func generateLeftSideIndexes(dimensions int) []int {
 	var indexes []int
 	cropped := dimensions / 2
 	for range dimensions {
@@ -14,7 +18,7 @@ func GenerateLeftSideIndexes(dimensions int) []int {
 	return indexes
 }
 
-func GenerateMiddleIndexes(dimensions int) []int {
+func generateMiddleIndexes(dimensions int) []int {
 	var indexes []int
 	for row := range dimensions {
 		if include := rand.Intn(2) == 1; include {
@@ -24,11 +28,18 @@ func GenerateMiddleIndexes(dimensions int) []int {
 	return indexes
 }
 
-func GenerateRightSideIndexes(leftSide []int, dimensions int) []int {
+func generateRightSideIndexes(leftSide []int, dimensions int) []int {
 	var indexes []int
 	cropped := dimensions / 2
 	for _, cellIndex := range leftSide {
 		indexes = append(indexes, cropped-cellIndex-1)
 	}
 	return indexes
+}
+
+func (p Pattern) Generate(dimensions int) Pattern {
+	left := generateLeftSideIndexes(dimensions)
+	middle := generateMiddleIndexes(dimensions)
+	right := generateRightSideIndexes(left, dimensions)
+	return Pattern{left, middle, right}
 }
