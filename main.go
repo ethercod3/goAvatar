e44e1f07819bb7231e4baec3db475d197e226425
Etherial <typicaluserinfinite@gmail.com>
2025-02-12 15:44:57 +0900

    adjust main.go

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package goAvatar
 
 import (
+	"image"
+
 	"github.com/ethercod3/goAvatar/colors"
 	"github.com/ethercod3/goAvatar/images"
 	"github.com/ethercod3/goAvatar/patterns"
 )
 
 type AvatarOptions struct {
-	Filepath               string
 	Dimensions, FileSizePx int
 }
 
-func GenerateAvatar(options AvatarOptions) error {
+func GenerateAvatar(options AvatarOptions) *image.RGBA {
 	scheme := colors.GenerateColorScheme()
 	pattern := patterns.GeneratePattern(options.Dimensions)
-	err := images.Draw(pattern, scheme, options.FileSizePx, options.Dimensions, options.Filepath)
-	return err
+	img := images.Draw(pattern, scheme, options.FileSizePx, options.Dimensions)
+	return img
 }
